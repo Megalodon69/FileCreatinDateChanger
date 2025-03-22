e4a9ad03c5a27c905e02ad7bf4f47383c1cc0bda
Sophie <128809667+Megalodon69@users.noreply.github.com>
2025-03-22 13:02:41 +0100

    Update CreationDateChanger.py
    Deleted Unnesesary comments

--- CreationDateChanger.py
+++ CreationDateChanger.py
@@ -1,22 +1,17 @@
 import os
 import win32api
 
-# Function to change file properties
 def change_file_properties(file_path, creation_time=None, author=None, last_modification_time=None):
-    # Set the file creation time
     if creation_time is not None:
         os.system('powershell.exe (Get-Item "{}").creationtime=("{}")'.format(file_path, creation_time))
     
-    # Set the file author
     if author is not None:
         win32api.SetFileAttributes(file_path, win32api.GetFileAttributes(file_path) & ~win32api.FILE_ATTRIBUTE_READONLY)
         os.system('powershell.exe (Get-Item "{}").author=("{}")'.format(file_path, author))
     
-    # Set the file last modification time
     if last_modification_time is not None:
         os.system('powershell.exe (Get-Item "{}").lastwritetime=("{}")'.format(file_path, last_modification_time))
 
-# Main function to prompt user for input and call change_file_properties()
 def main():
     file_path = input("Enter the file path: ")
     
